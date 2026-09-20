a65b77712a411a3012a22bd28b71853e81db5e9b
pandaysq <vova.sevostyanov.2009.12@gmail.com>
2026-09-20 12:28:39 +0500

    Add src/main/java/ru/core/accessories/state/ManaProvider.java

---
+++ src/main/java/ru/core/accessories/state/ManaProvider.java
@@ -0,0 +1,13 @@
+package ru.core.accessories.state;
+
+import org.bukkit.entity.Player;
+
+/**
+ * Источник маны для условий mana и mana_regen.
+ * Используется ConditionRegistry и заменяется будущей интеграцией с плагином маны.
+ * Для нового провайдера реализуйте два метода и передайте его в EffectEngine.
+ */
+public interface ManaProvider {
+    double getMana(Player player);
+    double getManaRegen(Player player);
+}
